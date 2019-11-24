ebce153f78111e2c599eae560130e14c39f69780
Shahwar Spall <shevy.spall@gmail.com>
2019-11-24 21:31:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+require "pry"
+class School
+ 
+  attr_reader :roster
+  
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = []
+  end 
+  
+  def add_student(name,grade)
+    @name[grade] = name 
+    @grade = grade 
+  
+    @roaster[grade] << name
+  end
+end 
